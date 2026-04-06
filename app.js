8b0aba86ded96c9fff10388c386f3cc72af98a4b
Ragheed173 <s12323203@stu.najah.edu>
2026-04-06 16:45:37 +0300

    14

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const requestSenderAxios = require('./requestSenderAxios');
+const chalk = require('chalk');
 
 async function main() {
-    const data = await requestSenderAxios.sendRequest();
-    console.log('data in app.js:', data);
-    return data;
+    // const data = await requestSenderAxios.sendRequest();
+    // console.log('data in app.js:', data);
+    // return data;
+    console.log('hello');
+    console.log(chalk.red('hello'));
 }
 
 main();
